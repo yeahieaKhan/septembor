2e65c8f1b3a6cbd519bf7b357b3eb3a44dd2b992
yeahieaKhan <114679737+yeahieaKhan@users.noreply.github.com>
2023-09-20 16:40:13 +0600

    Update task3.php

--- module_tow/task3.php
+++ module_tow/task3.php
@@ -16,27 +16,3 @@
     }
 
 ?> 
-
-
-<?php
-// Function to print the first n numbers in the Fibonacci series
-// function printFibonacci($n) {
-//     $first = 0;
-//     $second = 1;
-
-//     for ($i = 1; $i <= $n; $i++) {
-//         // Print the current Fibonacci number
-//         echo $first . " ";
-
-//         // Calculate the next Fibonacci number
-//         $next = $first + $second;
-
-//         // Update the values of $first and $second for the next iteration
-//         $first = $second;
-//         $second = $next;
-//     }
-// }
-
-// // Call the function to print the first 15 Fibonacci numbers
-// printFibonacci(5);
-// ?>
